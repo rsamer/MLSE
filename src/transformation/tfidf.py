fbd6a07570bf407fa1fdd62a01adbddeb1995b20
Michael Herold <michael.herold@student.tugraz.at>
2016-06-05 14:02:28 +0200

    TfidfVectorizer parameter changes

--- src/transformation/tfidf.py
+++ src/transformation/tfidf.py
@@ -11,20 +11,21 @@ def extract_tokens(post):
     return post.tokens()
 
 
-def tfidf(train_posts, test_posts, max_features=None, min_df=1):
+def tfidf(train_posts, test_posts, max_features=None, min_df=1, max_df=1.0):
     _logger.info("TFIDF-Vectorizer (Transformation)")
     #vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words='english')
     vectorizer = TfidfVectorizer(stop_words=None,
-                                 #ngram_range=(1, 3), #ngram_range has no impact when using our own tokenizer/preprocessor/analyzer
+                                 #ngram_range=(1, 3), # no impact when using our own tokenizer/preprocessor/analyzer
                                  preprocessor=extract_tokens,
                                  analyzer=extract_tokens,
                                  tokenizer=extract_tokens,
                                  #token_pattern=r'.*',
                                  min_df=min_df, # get rid of noise!
+                                 max_df=max_df,
                                  use_idf=True,
-                                 smooth_idf=False,
                                  sublinear_tf=False,
-                                 norm=None,
+                                 smooth_idf=True,
+                                 norm="l2",
                                  max_features=max_features)
 
     _logger.debug("Extracting features from the training data using a sparse vectorizer")
