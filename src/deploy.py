9e12dec67456d345bcd2506313494cd3ef286476
Ralph Samer <ralph.samer@student.tugraz.at>
2016-06-06 18:57:05 +0200

    Fixes formatted string issue

--- src/deploy.py
+++ src/deploy.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+    Quick & dirty deployment script
+'''
+
 import logging
 import os
 import sys
-import cmd
 import paramiko
 import getpass
 from scp import SCPClient
@@ -14,8 +17,6 @@ _logger = logging.getLogger(__name__)
 
 
 def setup_logging(log_level):
-    import platform
-    #import os
     logging.basicConfig(
         filename=None,
         level=log_level,
@@ -24,7 +25,7 @@ def setup_logging(log_level):
     )
 
 
-def createSSHClient(server, port, user, password):
+def create_ssh_client(server, port, user, password):
     client = paramiko.SSHClient()
     client.load_system_host_keys()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
@@ -78,8 +79,8 @@ def deploy(ssh_client, config):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        helper.error("usage: python deploy.py <hostname_or_ip> <port> <username> <deploy>")
+    if len(sys.argv) < 4:
+        helper.error("usage: python deploy.py <hostname_or_ip> <port> <username> [--not-deploy]")
 
     config = {
         "dirs_to_be_uploaded": [helper.APP_PATH],
@@ -109,24 +110,24 @@ if __name__ == "__main__":
         password = getpass.getpass("Please enter password: ")
 
     ssh_client = None
-    ssh_client = createSSHClient(sys.argv[1], int(sys.argv[2]), sys.argv[3], password)
+    ssh_client = create_ssh_client(sys.argv[1], int(sys.argv[2]), sys.argv[3], password)
     if ssh_client is None:
         helper.error("Deployment failed!")
 
-    if sys.argv[4].lower() != "--not-deploy":
+    if len(sys.argv) <= 4 or sys.argv[4].lower() != "--not-deploy":
         result = deploy(ssh_client, config)
         if not result:
             helper.error("Deployment failed!")
+        print "Deployment finished!"
 
-    print "Deployment finished!"
-
-    # TODO: execute job!
+    # execute job on server
     data_set = "example"
     #_, stdout, stderr = ssh_client.exec_command('cd MLSE/src/ && python -m main ../data/example')
-    _, stdout, stderr = ssh_client.exec_command('cd MLSE/src/ && screen -dmS MLSE.%s python -m main ../data/%s' % data_set)
+    _, stdout, stderr = ssh_client.exec_command('cd MLSE/src/ && screen -dmS MLSE.%s python -m main ../data/%s' % (data_set, data_set))
     for line in stdout:
         if line is not None and len(line) > 0:
             print line
     for line in stderr:
         if line is not None and len(line) > 0:
             print line
+    print "Started job in new screen"
