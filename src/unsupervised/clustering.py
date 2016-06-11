b90f41bd4cda8fd53993d5c469f2f133c58876f0
Michael Herold <michael.herold@student.tugraz.at>
2016-06-11 15:54:14 +0200

    Changed kMeans parameters

--- src/unsupervised/clustering.py
+++ src/unsupervised/clustering.py
@@ -13,26 +13,26 @@ _logger = logging.getLogger(__name__)
 def cluster(X_train, y_train, X_test, y_test, tags, n_suggested_tags, use_numeric_features):
     parameters = {
         'kmeans__n_clusters': (len(tags), int(len(tags) / 2)),
-        'kmeans__max_iter': (100,),
-        'kmeans__n_init': (10,),
-#         'kmeans__tol': (0.00004, 0.001),
+        'kmeans__max_iter': (100, ),
+        'kmeans__n_init': (10, ),
+        'kmeans__tol': (0.00004, 0.001)
     }
 
     if not use_numeric_features:
-#         parameters['vectorizer__max_features'] = (None, 1000, 3000, 10000)
-#         parameters['vectorizer__max_df'] = (0.75, 0.8, 1.0)
-#         parameters['vectorizer__min_df'] = (2, 3)
+        parameters['vectorizer__max_features'] = (None, 1000, 3000, 10000)
+        parameters['vectorizer__max_df'] = (0.75, 0.8, 1.0)
+        parameters['vectorizer__min_df'] = (2, 4)
         parameters['vectorizer__ngram_range'] = ((1, 1), (1, 2), (1, 3))  # unigrams, bigrams or trigrams
         parameters['tfidf__use_idf'] = (True,)
 
         pipeline = Pipeline([
             ('vectorizer', CountVectorizer()),
             ('tfidf', TfidfTransformer()),
-            ('kmeans', kmeans.CustomKMeans(init='k-means++', verbose=False, n_suggested_tags=n_suggested_tags))
+            ('kmeans', kmeans.CustomKMeans( init='k-means++', verbose=True))
         ])
     else:
         pipeline = Pipeline([
-            ('kmeans', kmeans.CustomKMeans(init='k-means++', verbose=True, n_suggested_tags=n_suggested_tags))
+            ('kmeans', kmeans.CustomKMeans(init='k-means++', verbose=True))
         ])
 
     pipeline = GridSearchCV(pipeline, parameters, n_jobs=-1, cv=3, verbose=0)
